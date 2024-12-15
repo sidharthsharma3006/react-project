8ba6795812a09993592b823839a0f7bd911291fa
sidharthsharma3006 <113471177+sidharthsharma3006@users.noreply.github.com>
2024-12-15 21:25:58 +0530

    Update Signup.jsx

--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -17,7 +17,7 @@ function Signup() {
      email: data.email, 
      password: data.password, 
     } 
-    await axios.post("http://localhost:3000/user/signup",userInfo).then((res)=>{
+    await axios.post("https://react-project-backend-0ijt.onrender.com/user/signup",userInfo).then((res)=>{
       console.log(res.data) 
       if(res.data){
           toast.success("Login Successfully");  
